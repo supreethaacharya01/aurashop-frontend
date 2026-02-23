5fdab21a5df5b672e75aad290bea9eb999492911
supreetha <supthaacharya@gmail.com>
2026-02-23 16:11:05 +0530

    Updated frontend API host to production backend

--- src/ContextProvider.jsx
+++ src/ContextProvider.jsx
@@ -3,6 +3,7 @@ import React , { createContext } from "react";
 export const UserContext=createContext();
 
 export default function ContextProvider({children}) {
+    // const host="http://localhost:7002"
     const host="https://aurashop-backend-s3ua.onrender.com"
   return (
     <div>
